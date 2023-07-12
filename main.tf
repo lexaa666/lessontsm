e28f70ed79f07eb1a7776d041e5e9e0b244bb18b
Aleksey Birulya <abirulya@mtbank.by>
2023-07-13 00:20:55 +0300

    add local-exec command param

--- main.tf
+++ main.tf
@@ -18,9 +18,7 @@ resource "aws_instance" "ec2_instance_ab_pub" {
   subnet_id = aws_subnet.ab_publicsubnet.id
 
   provisioner "local-exec" {
-    inline = [
-    "sudo apt-get install nginx -y"
-    ]
+    command = "sudo apt-get install nginx -y"
     }
   tags = {
     Name = "ab_tf_pub"
