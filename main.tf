8e2f8676c8e17fa8e77555b0f65e53bcacf23809
Aleksey Birulya <abirulya@mtbank.by>
2023-07-12 23:40:36 +0300

    edit name sg resource

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "ec2_instance_ab_pub" {
   }
 }
 
-resource "aws_network_interface_sg_attachment" "sg_attachment_ab" {
+resource "aws_network_interface_sg_attachment" "sg_attachment_pub_ab" {
   network_interface_id = aws_instance.ec2_instance_ab_pub.primary_network_interface_id
   security_group_id    = aws_security_group.allow_http_ssh_pub.id
 }
@@ -37,7 +37,7 @@ resource "aws_instance" "ec2_instance_ab_priv" {
   }
 }
 
-resource "aws_network_interface_sg_attachment" "sg_attachment_ab" {
+resource "aws_network_interface_sg_attachment" "sg_attachment_priv_ab" {
   network_interface_id = aws_instance.ec2_instance_ab_priv.primary_network_interface_id
   security_group_id    = aws_security_group.allow_http_ssh_priv.id
 }
