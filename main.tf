6cb96583f231d22eb59dd48cae2fa4438f494be2
Aleksey Birulya <abirulya@mtbank.by>
2023-07-12 23:36:01 +0300

    add sg priv and pub

--- main.tf
+++ main.tf
@@ -16,16 +16,28 @@ resource "aws_instance" "ec2_instance_ab_pub" {
   ami = "ami-04e601abe3e1a910f"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.ab_publicsubnet.id
+  security_groups {}
   tags = {
     Name = "ab_tf_pub"
   }
+}
+
+resource "aws_network_interface_sg_attachment" "sg_attachment_ab" {
+  network_interface_id = aws_instance.ec2_instance_ab_pub.primary_network_interface_id
+  security_group_id    = aws_security_group.allow_http_ssh_pub.id
 }
 
 resource "aws_instance" "ec2_instance_ab_priv" {
   ami = "ami-04e601abe3e1a910f"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.ab_privatesubnet
+  security_groups {}
   tags = {
     Name = "ab_tf_priv"
   }
+}
+
+resource "aws_network_interface_sg_attachment" "sg_attachment_ab" {
+  network_interface_id = aws_instance.ec2_instance_ab_priv.primary_network_interface_id
+  security_group_id    = aws_security_group.allow_http_ssh_priv.id
 }
--- sg.tf
+++ sg.tf
@@ -1,10 +1,41 @@
-resource "aws_security_group" "allow_http_ssh" {
+resource "aws_security_group" "allow_http_ssh_pub" {
   name        = "allow_http"
   description = "Allow http inbound traffic"
+  vpc_id = aws_vpc.ab_vpc.id
   ingress {
     description = "http"
     from_port   = 80
     to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "ssh"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+   tags = {
+    Name = "allow_http_ssh"
+  }
+}
+
+resource "aws_security_group" "allow_http_ssh_priv" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic"
+  vpc_id = aws_vpc.ab_vpc.id
+  ingress {
+    description = "http"
+    from_port   = 8080
+    to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
